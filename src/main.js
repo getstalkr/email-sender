3760dffdc8d4064583d695bb7510f9f047658975
Matheus Brasil <matheus.brasil10@gmail.com>
2017-03-18 05:09:50 -0300

    feat(core): get data from POST and send using our helper

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import micro from 'micro'
+import send from './helpers/sendMessage'
 
 const server = micro(
 
+  async function (req, res) {
 
+    const { subject, message, senderEmail } = await micro.json(req)
+
+    await send(subject, message, senderEmail)
+
+    return 'Successfully sent!'
+
+  }
 
 )
 
